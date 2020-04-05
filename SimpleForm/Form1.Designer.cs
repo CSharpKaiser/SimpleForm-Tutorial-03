a24407bfcfc7a075155a04e3961462dcb7052955
CSharp Kaiser <CSharpKaiser@hotmail.com>
2020-04-05 08:35:21 +0200

    Initial Commit

---
+++ SimpleForm/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SimpleForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.tbForename = new System.Windows.Forms.TextBox();
+            this.tbSurname = new System.Windows.Forms.TextBox();
+            this.lvPeople = new System.Windows.Forms.ListView();
+            this.colTitle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colForename = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colSurname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // bSave
+            // 
+            this.bSave.Location = new System.Drawing.Point(176, 173);
+            this.bSave.Name = "bSave";
+            this.bSave.Size = new System.Drawing.Size(130, 48);
+            this.bSave.TabIndex = 0;
+            this.bSave.Text = "Save";
+            this.bSave.UseVisualStyleBackColor = true;
+            this.bSave.Click += new System.EventHandler(this.bSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Forename";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Surname";
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Location = new System.Drawing.Point(176, 57);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.Size = new System.Drawing.Size(72, 20);
+            this.tbTitle.TabIndex = 4;
+            // 
+            // tbForename
+            // 
+            this.tbForename.Location = new System.Drawing.Point(176, 83);
+            this.tbForename.Name = "tbForename";
+            this.tbForename.Size = new System.Drawing.Size(175, 20);
+            this.tbForename.TabIndex = 5;
+            // 
+            // tbSurname
+            // 
+            this.tbSurname.Location = new System.Drawing.Point(176, 109);
+            this.tbSurname.Name = "tbSurname";
+            this.tbSurname.Size = new System.Drawing.Size(175, 20);
+            this.tbSurname.TabIndex = 6;
+            // 
+            // lvPeople
+            // 
+            this.lvPeople.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colTitle,
+            this.colForename,
+            this.colSurname});
+            this.lvPeople.HideSelection = false;
+            this.lvPeople.Location = new System.Drawing.Point(414, 44);
+            this.lvPeople.Name = "lvPeople";
+            this.lvPeople.Size = new System.Drawing.Size(325, 213);
+            this.lvPeople.TabIndex = 7;
+            this.lvPeople.UseCompatibleStateImageBehavior = false;
+            this.lvPeople.View = System.Windows.Forms.View.Details;
+            // 
+            // colTitle
+            // 
+            this.colTitle.Text = "Title";
+            // 
+            // colForename
+            // 
+            this.colForename.Text = "Forename";
+            this.colForename.Width = 100;
+            // 
+            // colSurname
+            // 
+            this.colSurname.Text = "Surname";
+            this.colSurname.Width = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvPeople);
+            this.Controls.Add(this.tbSurname);
+            this.Controls.Add(this.tbForename);
+            this.Controls.Add(this.tbTitle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.TextBox tbForename;
+        private System.Windows.Forms.TextBox tbSurname;
+        private System.Windows.Forms.ListView lvPeople;
+        private System.Windows.Forms.ColumnHeader colTitle;
+        private System.Windows.Forms.ColumnHeader colForename;
+        private System.Windows.Forms.ColumnHeader colSurname;
+    }
+}
+
---
+++ SimpleForm/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleForm.Model;
+
+namespace SimpleForm
+{
+    public partial class Form1 : Form
+    {
+        List<Person> personList;
+        public Form1()
+        {
+            InitializeComponent();
+            personList = new List<Person>();
+        }
+
+        private void bSave_Click(object sender, EventArgs e)
+        {
+            Person savePerson = new Person();
+            savePerson.Title = tbTitle.Text;
+            savePerson.Forename = tbForename.Text;
+            savePerson.Surname = tbSurname.Text;
+
+            //MessageBox.Show(savePerson.ToString());
+
+            personList.Add(savePerson);
+
+            ClearData();
+            Populate();
+        }
+
+        private void ClearData()
+        {
+            tbTitle.Text = "";
+            tbForename.Text = "";
+            tbSurname.Text = "";
+        }
+
+        private void Populate()
+        {
+            lvPeople.Items.Clear();
+            foreach (Person pep in personList)
+            {
+                ListViewItem item = new ListViewItem(pep.Title);
+                item.SubItems.Add(pep.Forename);
+                item.SubItems.Add(pep.Surname);
+
+                lvPeople.Items.Add(item);
+            }
+        }
+    }
+}
